f6c86d0fcc9df5569a182ca570d3db11b560bdd1
Ramomjcs <ramom1999@live.com>
2025-05-14 09:44:47 -0300

    Changing interfaces to models

--- src/app/features/heroes/models/hero.ts
+++ src/app/features/heroes/models/hero.ts
@@ -1,4 +1,14 @@
-export interface Hero {
-  id: number;
-  name: string;
+export class Hero {
+  constructor(
+    public id: number,
+    public name: string
+  ) {}
+
+  static fromPlain(obj: Partial<Hero>): Hero {
+    return new Hero(obj.id ?? 0, obj.name ?? '');
+  }
+
+  get displayName(): string {
+    return `${this.name} (#${this.id})`;
+  }
 }
